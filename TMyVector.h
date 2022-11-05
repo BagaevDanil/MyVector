4e68618780cf0aba18f6e454cc2ec17f0cb72e4b
BagaevDanil <MrBagaevDanil@yandex.ru>
2022-11-05 22:32:48 +0300

    add Constructor and Destructor

--- TMyVector.h
+++ TMyVector.h
@@ -4,5 +4,20 @@ class TMyVector
 public:
     //
 private:
-    //
+    T* ReservArr_;
+    int CurrentSize_;
+    int ReservSize_;
+    int MagFactor_ = 2;
+public:
+    TMyVector(int size) {
+        ReservSize_ = size;
+        CurrentSize_ = 0;
+        ReservArr_ = new T(ReservSize_);
+    }
+
+    TMyVector() : TMyVector(0) {}
+
+    ~TMyVector() {
+        delete[] ReservArr_;
+    }   
 };
