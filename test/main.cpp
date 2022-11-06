d3ec63ed89dfe56390a5b1edfd3cfe17b90e6635
BagaevDanil <MrBagaevDanil@yandex.ru>
2022-11-06 20:03:45 +0300

    add test Insert

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,30 @@
 #include "gtest/gtest.h"
+#include "../src/TMyVector.h"
 
-TEST(MyVector, PushBack_MayBe) {
-    ASSERT_EQ(1, 2);
+TEST(TMyVector, GetSize) {
+    TMyVector<int> vec(15);
+    EXPECT_EQ(vec.GetSize(), 0);
+}
+
+TEST(TMyVector, GetReservSize) {
+    TMyVector<int> vec(15);
+    EXPECT_EQ(vec.GetReservSize(), 15);
+}
+
+TEST(TMyVector, Insert) {
+    TMyVector<int> vec(15);
+    EXPECT_EQ(vec.GetSize(), 0);
+    EXPECT_EQ(vec.GetReservSize(), 15);
+
+    vec.Insert(0, 10); // 10
+    vec.Insert(0, 1); // 1 10
+    vec.Insert(1, 23); // 1 23 10
+    vec.Insert(3, 43); // 1 23 10 43
+
+    EXPECT_EQ(vec.GetValue(0), 1);
+    EXPECT_EQ(vec.GetValue(1), 23);
+    EXPECT_EQ(vec.GetValue(2), 10);
+    EXPECT_EQ(vec.GetValue(3), 43);
 }
 
 int main()
